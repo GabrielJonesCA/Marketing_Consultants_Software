4f976c69955a16a90a05fa91ab8af4aca3bb9236
Gabriel Jones <gabrieljones900@yahoo.com>
2023-11-21 20:23:06 -0800

    web page updates

--- Rebuild.py
+++ Rebuild.py
@@ -15,10 +15,7 @@ class App(customtkinter.CTk):
         self.title("Ad Assist Pro 2.0")
         self.geometry(f"{1100}x{580}")
 
-# configure grid layout (4x4)
-        self.grid_columnconfigure(1, weight=1)
-        self.grid_columnconfigure((2, 3), weight=0)
-        self.grid_rowconfigure((0, 1, 2), weight=1)
+
 
 if __name__ == "__main__":
     app = App()
